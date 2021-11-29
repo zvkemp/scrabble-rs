588c5e0588b99a8cdeaec891e07f6bb7dc4962f6
zvkemp <zvkemp@gmail.com>
2021-11-28 19:16:23 -0800

    basic game play

--- src/scrabble/mod.rs
+++ src/scrabble/mod.rs
@@ -4,8 +4,8 @@ use std::{
     str::FromStr,
 };
 
-use rand::seq::SliceRandom;
-use rand::thread_rng;
+use rand::{prelude::StdRng, thread_rng, SeedableRng};
+use rand::{seq::SliceRandom, Rng};
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
@@ -14,16 +14,184 @@ pub struct Game {
     players: Vec<Player>,
     player_index: usize,
     bag: Bag,
+    racks: Vec<Rack>,
+    scores: Vec<Vec<TurnScore>>,
+    state: State,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
+enum State {
+    Pre,
+    Started,
+    Over,
+}
+
+impl Default for State {
+    fn default() -> Self {
+        Self::Pre
+    }
+}
+
+impl std::fmt::Debug for Game {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("Game")
+            .field("players", &self.players)
+            .field("racks", &self.racks)
+            .field("player_index", &self.player_index)
+            .field("bag", &self.bag)
+            .field("scores", &self.scores)
+            .field("state", &self.state)
+            .finish()?;
+
+        f.write_str("\n")?;
+        f.write_str(&self.board.as_board_string())
+    }
+}
+
+// FIXME: this should also have a uuid
+#[derive(Debug, Serialize, Deserialize, Eq, PartialEq, Hash)]
 pub struct Player(String);
+
+pub type Rack = Vec<Tile>;
 
 impl Game {
     pub fn check_complete() {
         todo!()
     }
+
+    pub fn start(&mut self) -> Result<(), Error> {
+        self.init_racks()?;
+        self.init_player_index()?;
+        self.init_scores()?;
+        self.state = State::Started;
+        Ok(())
+    }
+
+    pub fn is_over(&self) -> bool {
+        if let State::Over = self.state {
+            true
+        } else {
+            false
+        }
+    }
+
+    fn init_scores(&mut self) -> Result<(), Error> {
+        for _ in &self.players {
+            self.scores.push(Vec::new())
+        }
+        Ok(())
+    }
+
+    // FIXME ensure players are unique
+    pub fn add_player(&mut self, player: Player) -> Result<(), Error> {
+        self.players.push(player);
+        Ok(())
+    }
+
+    fn init_racks(&mut self) -> Result<(), Error> {
+        for index in 0..self.players.len() {
+            self.racks.push(Rack::default());
+            self.fill_rack_at(index)?;
+        }
+
+        Ok(())
+    }
+
+    fn fill_rack_at(&mut self, index: usize) -> Result<(), Error> {
+        let rack = &mut self.racks[index];
+
+        while rack.len() < 7 {
+            match self.bag.pop() {
+                None => {
+                    return Ok(());
+                }
+                Some(tile) => rack.push(tile),
+            }
+        }
+
+        Ok(())
+    }
+
+    fn init_player_index(&mut self) -> Result<(), Error> {
+        self.player_index = thread_rng().gen_range(0..self.players.len());
+        Ok(())
+    }
+
+    pub fn play(&mut self, turn: Turn) -> Result<(), Error> {
+        match self.state {
+            State::Pre => return Err(Error::NotStarted),
+            State::Over => return Err(Error::GameOver),
+            _ => (),
+        }
+        // FIXME: make this an atomic operation? Need something like immutable
+        self.validate_turn(&turn)?;
+        self.score_turn(&turn)?;
+        self.spend_tiles(&turn)?;
+        self.board.commit_turn(&turn)?;
+        self.fill_rack_at(self.player_index)?;
+        self.next_player()?;
+        self.check_game_over()?;
+        Ok(())
+    }
+
+    fn check_game_over(&mut self) -> Result<(), Error> {
+        if self.bag.0.is_empty() && self.racks.iter().any(|r| r.is_empty()) {
+            self.state = State::Over;
+        }
+
+        Ok(())
+    }
+
+    fn validate_turn(&mut self, turn: &Turn) -> Result<(), Error> {
+        turn.validate()?;
+        // FIXME: any way to do this once? This clone currently happens again in the commit.
+        Self::spend_tiles_inner(turn, self.racks[self.player_index].clone())?;
+        // FIXME: validate connected to other words, or on space 112 (initial turn)
+        // FIXME: validate that turn indexes aren't already occupied
+        // FIXME: validate words in dictionary
+        Ok(())
+    }
+
+    fn score_turn(&mut self, turn: &Turn) -> Result<(), Error> {
+        let score = self.board.score_turn(turn)?;
+        self.scores[self.player_index].push(score);
+
+        Ok(())
+    }
+
+    // advance cursor to next player
+    fn next_player(&mut self) -> Result<(), Error> {
+        self.player_index += 1;
+        self.player_index %= self.players.len();
+        Ok(())
+    }
+
+    fn spend_tiles(&mut self, turn: &Turn) -> Result<(), Error> {
+        let mut new_rack = Self::spend_tiles_inner(turn, self.racks[self.player_index].clone())?;
+        std::mem::swap(&mut self.racks[self.player_index], &mut new_rack);
+
+        Ok(())
+    }
+
+    fn spend_tiles_inner(turn: &Turn, mut rack: Rack) -> Result<Rack, Error> {
+        for (_, tile) in &turn.tiles {
+            // FIXME: handle blanks
+            let index = rack
+                .iter()
+                .position(|rack_tile| rack_tile == tile)
+                .ok_or_else(|| Error::NoTileToSpend(*tile))?;
+
+            rack.remove(index);
+        }
+        Ok(rack)
+    }
     // FIXME: allow up to two incorrect submissions before turn ends
+}
+
+impl From<&str> for Player {
+    fn from(name: &str) -> Self {
+        Player(name.to_owned())
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -45,6 +213,9 @@ impl Default for Game {
             players: Default::default(),
             player_index: 0,
             bag: Bag::standard(),
+            racks: Default::default(),
+            scores: Default::default(),
+            state: Default::default(),
         }
     }
 }
@@ -83,6 +254,10 @@ macro_rules! l {
 }
 
 impl Bag {
+    pub fn pop(&mut self) -> Option<Tile> {
+        self.0.pop()
+    }
+
     pub fn standard() -> Self {
         let counts = vec![
             (l!('A'), 9),
@@ -128,9 +303,14 @@ impl Bag {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum Error {
     BoardParse(String),
+    NoTileToSpend(Tile),
+    TurnIndexesNotUnique,
+    TurnNotLinear,
+    NotStarted,
+    GameOver,
 }
 
 impl Board {
@@ -186,23 +366,27 @@ impl Board {
     }
 
     // FIXME: check dictionary and return Result instead
-    fn new_words(&self, turn: &Turn) -> HashSet<Word> {
-        let original: HashSet<Word> = self.words().collect();
+    fn new_words(&self, turn: &Turn) -> Vec<Word> {
+        let original: Vec<Word> = self.words().collect();
         let overlay = Overlay { board: self, turn };
         let horizontal = Words::horizontal(&overlay);
         let vertical = Words::vertical(&overlay);
-        let overlay_words: HashSet<Word> = horizontal.chain(vertical).collect();
+        let mut overlay_words: Vec<Word> = horizontal.chain(vertical).collect();
 
-        overlay_words.sub(&original)
+        for word in original {
+            overlay_words.retain(|w| *w != word);
+        }
+
+        overlay_words
     }
 
-    fn score_turn(&self, turn: &Turn) -> Result<Vec<(String, usize)>, Error> {
+    fn score_turn(&self, turn: &Turn) -> Result<TurnScore, Error> {
         let mut scores = vec![];
         for word in self.new_words(turn) {
             scores.push((String::from(&word), self.score_word(&word)))
         }
 
-        Ok(scores)
+        Ok(TurnScore { scores })
     }
 
     fn score_word(&self, word: &Word) -> usize {
@@ -244,9 +428,9 @@ impl Board {
 
     fn commit_turn(&mut self, turn: &Turn) -> Result<(), Error> {
         // FIXME: ensure turn is valid, get scores
-        for (index, char) in &turn.chars {
+        for (index, tile) in &turn.tiles {
             let entry = &mut self.0[*index];
-            let mut new_value = Square::Tile(Tile::Char(*char));
+            let mut new_value = Square::Tile(tile.clone());
             std::mem::swap(entry, &mut new_value);
         }
 
@@ -336,11 +520,12 @@ impl GetChar for Overlay<'_> {
 
 impl GetChar for Turn {
     fn get_char(&self, index: usize) -> Option<char> {
-        self.chars
+        self.tiles
             .iter()
             .filter(|(i, _)| *i == index)
-            .map(|(_, c)| *c)
+            .map(|(_, tile)| tile.as_char())
             .next()
+            .unwrap_or(None)
     }
 }
 
@@ -412,10 +597,6 @@ impl<S: GetChar> Iterator for Words<'_, S> {
 
             while let Some(char) = self.source.get_char(self.index) {
                 current.push(self.index, char);
-                println!(
-                    "current = {:?} (cursor={} ({}))",
-                    current, self.cursor, self.index
-                );
                 self.advance();
 
                 // end of row
@@ -429,7 +610,6 @@ impl<S: GetChar> Iterator for Words<'_, S> {
             } else {
                 self.advance();
                 current.clear();
-                println!("{:?} not long enough", current);
             }
         }
     }
@@ -477,7 +657,7 @@ fn transpose_index(index: usize, direction: &Direction) -> usize {
 }
 
 pub struct Turn {
-    chars: Vec<(usize, char)>,
+    tiles: Vec<(usize, Tile)>,
     // map of indexes to letters
     // player
     // validations:
@@ -489,22 +669,37 @@ pub struct Turn {
     // score!
 }
 
+#[derive(Deserialize, Serialize, Debug, Eq, PartialEq)]
 pub struct TurnScore {
-    words: Vec<(String, usize)>,
+    scores: Vec<(String, usize)>,
 }
 
 impl Turn {
     fn indexes(&self) -> impl Iterator<Item = &usize> {
-        self.chars.iter().map(|(i, _)| i)
+        self.tiles.iter().map(|(i, _)| i)
     }
 
-    fn validate_unique_indexes(&self) -> bool {
+    fn validate(&self) -> Result<(), Error> {
+        self.validate_unique_indexes()?;
+        self.validate_linear()?;
+
+        Ok(())
+    }
+
+    fn validate_unique_indexes(&self) -> Result<(), Error> {
         // all indexes should be unique
-        todo!()
+        if self.indexes().collect::<Vec<_>>().len()
+            == self.indexes().collect::<HashSet<&usize>>().len()
+        {
+            Ok(())
+        } else {
+            Err(Error::TurnIndexesNotUnique)
+        }
     }
 
-    fn validate_linear(&self) -> bool {
-        self.indexes()
+    fn validate_linear(&self) -> Result<(), Error> {
+        if self
+            .indexes()
             .map(|i| i % BOARD_SIZE)
             .collect::<HashSet<usize>>()
             .len()
@@ -515,6 +710,11 @@ impl Turn {
                 .collect::<HashSet<usize>>()
                 .len()
                 == 1
+        {
+            Ok(())
+        } else {
+            Err(Error::TurnNotLinear)
+        }
     }
 }
 
@@ -609,10 +809,10 @@ mod test {
     fn test_board_new_words() {
         let board = Board::parse(test_board_a()).unwrap();
         let turn = Turn {
-            chars: vec![(111, 'S'), (126, 'L'), (156, 'T')],
+            tiles: vec![(111, l!('S')), (126, l!('L')), (156, l!('T'))],
         };
 
-        assert!(turn.validate_linear());
+        assert!(turn.validate_linear().is_ok());
         // let turn_score = board.score_turn(&turn);
 
         let new_words: HashSet<String> = board.new_words(&turn).iter().map(Into::into).collect();
@@ -625,11 +825,15 @@ mod test {
     fn test_board_score_turn() {
         let board = Board::parse(test_board_a()).unwrap();
         let turn = Turn {
-            chars: vec![(111, 'S'), (126, 'L'), (156, 'T')],
+            tiles: vec![(111, l!('S')), (126, l!('L')), (156, l!('T'))],
         };
 
-        let scores: HashSet<(String, usize)> =
-            board.score_turn(&turn).unwrap().into_iter().collect();
+        let scores: HashSet<(String, usize)> = board
+            .score_turn(&turn)
+            .unwrap()
+            .scores
+            .into_iter()
+            .collect();
 
         assert_eq!(
             scores,
@@ -643,13 +847,11 @@ mod test {
     fn test_board_commit_turn() {
         let mut board = Board::parse(test_board_a()).unwrap();
         let turn = Turn {
-            chars: vec![(111, 'S'), (126, 'L'), (156, 'T')],
+            tiles: vec![(111, l!('S')), (126, l!('L')), (156, l!('T'))],
         };
 
         board.commit_turn(&turn).unwrap();
 
-        println!("{:?}", board);
-        println!("{}", board.as_board_string());
         let words: Vec<String> = board.words().map(Into::into).collect();
 
         let expected: Vec<String> = [
@@ -663,7 +865,144 @@ mod test {
     }
 
     #[test]
+    fn test_game_init() {
+        let mut game = Game::default();
+        game.add_player(Player::from("Frankie")).unwrap();
+        game.add_player(Player::from("Ada")).unwrap();
+        game.start().unwrap();
+
+        assert_eq!(game.racks.len(), 2);
+        assert_eq!(game.racks[0].len(), 7);
+        assert_eq!(game.racks[1].len(), 7);
+    }
+
+    fn test_bag() -> Bag {
+        let bag = vec![
+            l!('Q'),
+            l!('A'),
+            l!('P'),
+            l!('S'),
+            l!('T'),
+            l!('I'),
+            l!('E'),
+            l!('X'),
+            l!('L'),
+            l!('I'),
+            l!('T'),
+            l!('R'),
+            l!('A'),
+            l!('M'),
+            l!('S'),
+        ];
+
+        Bag(bag)
+    }
+
+    #[test]
     fn test_game_play() {
-        let game = Game::default();
+        let mut game = Game::default();
+        game.bag = test_bag();
+        game.add_player(Player::from("Frankie")).unwrap();
+        game.add_player(Player::from("Ada")).unwrap();
+
+        game.start().unwrap();
+        game.player_index = 0;
+
+        assert_eq!(game.racks.len(), 2);
+        assert_eq!(game.racks[0].len(), 7);
+        assert_eq!(game.racks[1].len(), 7);
+
+        println!("{:#?}", game);
+
+        let turn_a = Turn {
+            tiles: vec![(112, l!('M')), (113, l!('A')), (114, l!('R'))],
+        };
+        game.play(turn_a).unwrap();
+        println!("{:#?}", game);
+
+        assert_eq!(
+            game.racks[0],
+            vec![l!('S'), l!('T'), l!('I'), l!('L'), l!('Q')]
+        );
+
+        let words: Vec<String> = game.board.words().map(Into::into).collect();
+        assert_eq!(game.player_index, 1);
+        assert_eq!(words, vec!["MAR".to_string()]);
+
+        assert_eq!(
+            game.scores[0],
+            vec![TurnScore {
+                scores: vec![("MAR".to_owned(), 10)]
+            }]
+        );
+
+        let turn_b = Turn {
+            tiles: vec![(126, l!('T')), (127, l!('A')), (128, l!('X'))],
+        };
+
+        game.play(turn_b).unwrap();
+        println!("{:#?}", game);
+
+        assert_eq!(game.racks[1], vec![l!('E'), l!('I'), l!('S'), l!('P')]);
+
+        assert_eq!(
+            game.scores[1],
+            vec![TurnScore {
+                scores: vec![
+                    ("TAX".to_string(), 19),
+                    ("MA".to_string(), 4),
+                    ("AX".to_string(), 17),
+                ]
+            }]
+        );
+        assert_eq!(game.player_index, 0);
+
+        let turn_c_err_1 = Turn {
+            tiles: vec![(140, l!('T')), (127, l!('A')), (128, l!('X'))],
+        };
+
+        assert_eq!(game.play(turn_c_err_1).unwrap_err(), Error::TurnNotLinear);
+
+        let turn_c_err_2 = Turn {
+            tiles: vec![(140, l!('T')), (141, l!('A')), (142, l!('X'))],
+        };
+
+        assert_eq!(
+            game.play(turn_c_err_2).unwrap_err(),
+            Error::NoTileToSpend(l!('A'))
+        );
+
+        let turn_c_1 = Turn {
+            tiles: vec![(141, l!('I')), (156, l!('L'))],
+        };
+
+        game.play(turn_c_1).unwrap();
+        println!("{:#?}", game);
+
+        assert_eq!(
+            game.scores[0],
+            vec![
+                TurnScore {
+                    scores: vec![("MAR".to_owned(), 10)]
+                },
+                TurnScore {
+                    scores: vec![("TIL".to_owned(), 3)]
+                }
+            ]
+        );
+
+        let turn_d = Turn {
+            tiles: vec![
+                (169, l!('P')),
+                (170, l!('I')),
+                (171, l!('E')),
+                (172, l!('S')),
+            ],
+        };
+
+        game.play(turn_d).unwrap();
+        println!("{:#?}", game);
+
+        assert!(game.is_over());
     }
 }
