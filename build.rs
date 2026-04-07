b31a2adffb65bc8b0175fd9baf177eb64ff81d42
zvkemp <zvkemp@gmail.com>
2026-04-07 14:33:39 -0400

    wip

--- build.rs
+++ build.rs
@@ -5,6 +5,10 @@ fn main() {
         .output()
         .expect("failed to build js");
 
-    println!("cargo:rerun-if-changed=js/index.ts");
+    println!("cargo:rerun-if-changed=js");
+    println!("cargo:rerun-if-changed=css");
+    println!("cargo:rerun-if-changed=package.json");
+    println!("cargo:rerun-if-changed=yarn.lock");
+    println!("cargo:rerun-if-changed=tsconfig.json");
     println!("cargo:rerun-if-changed=build.rs");
 }
